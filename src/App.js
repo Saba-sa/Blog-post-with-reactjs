d8e6f961cfa131410106ce20235171f2a58809c6
saba ali <106993581+Saba-sa@users.noreply.github.com>
2023-12-07 11:08:49 +0500

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect } from 'react';
+import Header from './Blog/Header/Header.js';
+import Edit from './Blog/Edit/Edit.js';
+import Navbar from './Blog/Navbar/Navbar.js';
+import Home from './Blog/Home/Home.js';
+import NewPost from './Blog/NewPost/NewPost.js';
+import PostPage from './Blog/PostPage/PostPage.js';
+import Footer from './Blog/Footer/Footer.js';
+import About from './Blog/About/About.js';
+import Missing from './Blog/Missing/Missing.js';
+import './App.css'
+import { Route, Routes} from 'react-router-dom';
+import useAxiosFetch from './hooks/useAxiosFetch.js';
+import { useStoreActions } from 'easy-peasy';
 
 function App() {
+  const setPost=useStoreActions((actions)=>actions.setPosts)
+  const {data,fetchError,isLOading}=useAxiosFetch('http://localhost:3500/posts')
+  useEffect(()=>{
+  setPost(data);
+  },[data,setPost])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='app'>
+          <Header title="React Js Blog" />
+        <Navbar/> 
+        <Routes>
+          <Route path="/" element={<Home fetchError={fetchError} isLOading={isLOading}/>}/>
+          <Route path="/post" element={<NewPost/>}/>
+          <Route path="/post/:id" element={<PostPage/>}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/edit/:id" element={<Edit/>}/>
+          <Route path="*" element={<Missing />} />
+        </Routes>
+        <Footer />
+      </div>
   );
 }
 
